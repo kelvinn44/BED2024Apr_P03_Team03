7824cc254efcf346102d4395e81cec24c6206dd3
kelvinn44 <s10262567@connect.np.edu.sg>
2024-05-30 09:42:44 +0800

    index draft and added some more packages

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const staticMiddleware = express.static("public"); // Path to the public folder
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
 
+//bootstrap
+app.use("/",express.static("./node_modules/bootstrap/dist/"));
+
 app.use(staticMiddleware); // Mount the static middleware
 
 
@@ -19,24 +22,24 @@ app.use(staticMiddleware); // Mount the static middleware
 
 
 app.listen(port, async () => {
-  try {
-    // Connect to the database
-    await sql.connect(dbConfig);
-    console.log("Database connection established successfully");
-  } catch (err) {
-    console.error("Database connection error:", err);
-    // Terminate the application with an error code (optional)
-    process.exit(1); // Exit with code 1 indicating an error
-  }
+  // try {
+  //   // Connect to the database
+  //   await sql.connect(dbConfig);
+  //   console.log("Database connection established successfully");
+  // } catch (err) {
+  //   console.error("Database connection error:", err);
+  //   // Terminate the application with an error code (optional)
+  //   process.exit(1); // Exit with code 1 indicating an error
+  // }
 
   console.log(`Server listening on port ${port}`);
 });
 
 // Close the connection pool on SIGINT signal
-process.on("SIGINT", async () => {
-  console.log("Server is gracefully shutting down");
-  // Perform cleanup tasks (e.g., close database connections)
-  await sql.close();
-  console.log("Database connection closed");
-  process.exit(0); // Exit with code 0 indicating successful shutdown
-});
+// process.on("SIGINT", async () => {
+//   console.log("Server is gracefully shutting down");
+//   // Perform cleanup tasks (e.g., close database connections)
+//   await sql.close();
+//   console.log("Database connection closed");
+//   process.exit(0); // Exit with code 0 indicating successful shutdown
+// });
