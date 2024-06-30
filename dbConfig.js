0e43f0adf88a52d84c2cb9b56d85e153140da29d
kelvinn44 <s10262567@connect.np.edu.sg>
2024-06-30 11:57:30 +0800

    revert back to default dbConfig.js file and added dbConfig.js in .gitignore

--- dbConfig.js
+++ dbConfig.js
@@ -1,15 +1,11 @@
 module.exports = {
-   user: "user1", // Replace with your SQL Server login username
-   password: "Strawberrycake11", // Replace with your SQL Server login password
-   server: "localhost",
-  database: "bed_assg1",
-   trustServerCertificate: true,
-   options: {
-     port: 1433, // Default SQL Server port
-     connectionTimeout: 60000, // Connection timeout in milliseconds
-   },
+  user: "username", // Replace with your SQL Server login username
+  password: "password", // Replace with your SQL Server login password
+  server: "localhost",
+  database: "BED_Assignment_DB",
+  trustServerCertificate: true,
+  options: {
+    port: 1433, // Default SQL Server port
+    connectionTimeout: 60000, // Connection timeout in milliseconds
+  },
 };
-
-
-
-
