5bac3db09f290c7de47f105dc78ee34d26a953dd
Yuxin Natalie Wong <s10257861@connect.np.edu.sg>
2024-07-11 11:39:15 +0800

    validateForum done

---
+++ middlewares/validateUserForum.js
@@ -0,0 +1,21 @@
+const Joi = require("joi");
+
+const validateUser = (req, res, next) => {
+  const schema = Joi.object({
+    title: Joi.string().min(1).max(50).required(),
+    content: Joi.string().min(1).max(250).required(),
+   
+  });
+
+  const validation = schema.validate(req.body, { abortEarly: false }); // Validate request body
+
+  if (validation.error) {
+    const errors = validation.error.details.map((error) => error.message);
+    res.status(400).json({ message: "Validation error", errors });
+    return; // Terminate middleware execution on validation error
+  }
+
+  next(); // If validation passes, proceed to the next route handler
+};
+
+module.exports = validateUser;
