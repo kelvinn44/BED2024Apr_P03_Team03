2809fa1c6806f267480234359963eecceb6e0881
Yuxin Natalie Wong <s10257861@connect.np.edu.sg>
2024-07-18 18:52:50 +0800

    forum js

---
+++ public/scripts/forum-signedIn.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user && user.role === 'ForumMod') {
+        document.getElementById('admin-name').textContent = user.firstname;
+    } else {
+        window.location.href = "login.html";
+    }
+
+    document.getElementById("logout").addEventListener("click", (event) => {
+        event.preventDefault();
+        localStorage.removeItem("user");
+        alert("Successfully logged out!\nSee you again.");
+        window.location.href = "index.html";
+    });
+
+    // Call the displayEvents function when the page loads
+    display();
+});
