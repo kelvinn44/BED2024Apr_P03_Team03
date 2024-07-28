6dfbf4bbd45eb057efc75bf4d72042e71d03d2a8
Tan Guo Zhi Kelvin <148330557+kelvinn44@users.noreply.github.com>
2024-07-28 14:41:47 +0800

    Update root dbConfig.js back to default value

--- dbConfig.js
+++ dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  user: "user1", // Replace with your SQL Server login username
-  password: "Strawberrycake11", // Replace with your SQL Server login password
+  user: "username", // Replace with your SQL Server login username
+  password: "password", // Replace with your SQL Server login password
   server: "localhost",
-  database: "bed_assg1",
+  database: "BED_Assignment_DB",
   trustServerCertificate: true,
   options: {
     port: 1433, // Default SQL Server port
